5374e44db22e30122e3b6356c95e43085f9edae4
messickr <rnmessick@gmail.com>
2019-07-03 09:55:48 -0700

    feature 1 complete

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # lab-02
 
-Number and name of feature: ________________________________
+Number and name of feature: Feature #1 Display Images
 
-Estimate of time needed to complete: _____
+Estimate of time needed to complete: ____1 hr_
 
-Start time: _____
+Start time: ___915am__
 
-Finish time: _____
+Finish time: __954am___
 
-Actual time needed to complete: _____
+Actual time needed to complete: ___40 min__
+
--- js/app.js
+++ js/app.js
@@ -1,4 +1,49 @@
 'use strict';
+/*global $ */
+
+const HornedAnimal = function(image_url, title, description, keyword, horns) {
+  this.image_url = image_url;
+  this.title = title;
+  this.description = description;
+  this.keyword = keyword;
+  this.horns = horns;
+
+  HornedAnimal.allAnimals.push(this);
+};
+
+HornedAnimal.allAnimals = [];
+
+HornedAnimal.prototype.renderWithJquery = function(){
+  const $newAnimal = $('<section></section');
+
+  const animalTemplateHTML = $('#photo-template').html();
+
+  $newAnimal.html(animalTemplateHTML);
+
+  $newAnimal.find('h2').text(this.title);
+  $newAnimal.find('img').attr('src', this.image_url);
+  $newAnimal.find('p').text(this.description);
+
+  $('main').append($newAnimal);
+};
+
+
+HornedAnimal.getAllAnimals = function(){
+  $.get('../data/page-1.json', 'json').then(animalJSON => {
+    animalJSON.forEach(animal => {
+      new HornedAnimal(animal.image_url, animal.title, animal.description, animal.keyword, animal.horns);
+    });
+    //TODO: build render function
+
+    this.allAnimals.forEach(animal => {
+      animal.renderWithJquery();
+    });
+  });
+};
+
+HornedAnimal.getAllAnimals();
+
+
 
 
 
